a045744718d7665146570d5dd2729ae872c18d90
Kasim Mohammed <1992kasimm@gmail.com>
2019-06-15 20:41:30 +0300

    Add project files.

---
+++ WindowsFormsApplication2/Form1.Designer.cs
@@ -0,0 +1,102 @@
+﻿namespace WindowsFormsApplication2
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.txtWord = new System.Windows.Forms.TextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.txtNumbers = new System.Windows.Forms.TextBox();
+            this.SuspendLayout();
+            // 
+            // txtWord
+            // 
+            this.txtWord.Font = new System.Drawing.Font("Arial", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtWord.Location = new System.Drawing.Point(12, 180);
+            this.txtWord.Name = "txtWord";
+            this.txtWord.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.txtWord.Size = new System.Drawing.Size(1974, 40);
+            this.txtWord.TabIndex = 2;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Arial", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(1884, 43);
+            this.label1.Name = "label1";
+            this.label1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.label1.Size = new System.Drawing.Size(87, 37);
+            this.label1.TabIndex = 3;
+            this.label1.Text = "أرقام :";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Font = new System.Drawing.Font("Arial", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label2.Location = new System.Drawing.Point(1874, 140);
+            this.label2.Name = "label2";
+            this.label2.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.label2.Size = new System.Drawing.Size(97, 37);
+            this.label2.TabIndex = 4;
+            this.label2.Text = "الكلمة :";
+            // 
+            // txtNumbers
+            // 
+            this.txtNumbers.Font = new System.Drawing.Font("Arial", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.txtNumbers.Location = new System.Drawing.Point(12, 83);
+            this.txtNumbers.Name = "txtNumbers";
+            this.txtNumbers.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
+            this.txtNumbers.Size = new System.Drawing.Size(1974, 40);
+            this.txtNumbers.TabIndex = 1;
+            this.txtNumbers.TextChanged += new System.EventHandler(this.txtNumbers_TextChanged);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 19F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1988, 262);
+            this.Controls.Add(this.txtNumbers);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.txtWord);
+            this.Name = "Form1";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+        private System.Windows.Forms.TextBox txtWord;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.TextBox txtNumbers;
+    }
+}
+
