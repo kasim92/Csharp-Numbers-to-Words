a002ee0f21fa28a0723541e31a4e16e958fbba19
kasim mohammed <1992kasimm@gmail.com>
2019-07-06 00:35:06 +0300

    Revert "تصحيح المراتب"
    This reverts commit 920d3360b53c70c59038ade5ebacfec8b37c6fd7.

--- WindowsFormsApplication2/Form1.Designer.cs
+++ WindowsFormsApplication2/Form1.Designer.cs
@@ -38,22 +38,20 @@
             // 
             this.txtWord.Enabled = false;
             this.txtWord.Font = new System.Drawing.Font("Arial", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtWord.Location = new System.Drawing.Point(11, 123);
-            this.txtWord.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtWord.Location = new System.Drawing.Point(12, 180);
             this.txtWord.Name = "txtWord";
             this.txtWord.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
-            this.txtWord.Size = new System.Drawing.Size(1263, 29);
+            this.txtWord.Size = new System.Drawing.Size(1974, 40);
             this.txtWord.TabIndex = 2;
             // 
             // label1
             // 
             this.label1.AutoSize = true;
             this.label1.Font = new System.Drawing.Font("Arial", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label1.Location = new System.Drawing.Point(1214, 29);
-            this.label1.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label1.Location = new System.Drawing.Point(1884, 43);
             this.label1.Name = "label1";
             this.label1.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
-            this.label1.Size = new System.Drawing.Size(60, 26);
+            this.label1.Size = new System.Drawing.Size(87, 37);
             this.label1.TabIndex = 3;
             this.label1.Text = "أرقام :";
             // 
@@ -61,35 +59,32 @@
             // 
             this.label2.AutoSize = true;
             this.label2.Font = new System.Drawing.Font("Arial", 16F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.label2.Location = new System.Drawing.Point(1214, 95);
-            this.label2.Margin = new System.Windows.Forms.Padding(2, 0, 2, 0);
+            this.label2.Location = new System.Drawing.Point(1874, 140);
             this.label2.Name = "label2";
             this.label2.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
-            this.label2.Size = new System.Drawing.Size(66, 26);
+            this.label2.Size = new System.Drawing.Size(97, 37);
             this.label2.TabIndex = 4;
             this.label2.Text = "الكلمة :";
             // 
             // txtNumbers
             // 
             this.txtNumbers.Font = new System.Drawing.Font("Arial", 14F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
-            this.txtNumbers.Location = new System.Drawing.Point(11, 57);
-            this.txtNumbers.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
+            this.txtNumbers.Location = new System.Drawing.Point(12, 83);
             this.txtNumbers.Name = "txtNumbers";
             this.txtNumbers.RightToLeft = System.Windows.Forms.RightToLeft.Yes;
-            this.txtNumbers.Size = new System.Drawing.Size(1263, 29);
+            this.txtNumbers.Size = new System.Drawing.Size(1974, 40);
             this.txtNumbers.TabIndex = 1;
             this.txtNumbers.TextChanged += new System.EventHandler(this.txtNumbers_TextChanged);
             // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(9F, 19F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(1285, 179);
+            this.ClientSize = new System.Drawing.Size(1988, 262);
             this.Controls.Add(this.txtNumbers);
             this.Controls.Add(this.label2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.txtWord);
-            this.Margin = new System.Windows.Forms.Padding(2, 2, 2, 2);
             this.Name = "Form1";
             this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
             this.Text = "Form1";
