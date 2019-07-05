920d3360b53c70c59038ade5ebacfec8b37c6fd7
kasim mohammed <kasimm1992@hotmail.com>
2019-07-06 00:25:08 +0300

    تصحيح المراتب

--- WindowsFormsApplication2/Form1.cs
+++ WindowsFormsApplication2/Form1.cs
@@ -63,13 +63,15 @@ namespace WindowsFormsApplication2
                         switch (lengthAfterPoint)
                         {
                             case 3://بالمئة
-                                txtWord.Text = wordBeforePoint + " و " + wordAfterPoint + " بالمئة ";
-                                break;
-                            case 4://بالألف
-                            case 5:
-                            case 6:
                                 txtWord.Text = wordBeforePoint + " و " + wordAfterPoint + " بالألف ";
                                 break;
+                            case 4:
+                            txtWord.Text = wordBeforePoint + " و " + wordAfterPoint + " بالعشرة الالف ";
+                            break;
+                            case 5:
+                            txtWord.Text = wordBeforePoint + " و " + wordAfterPoint + " بالمئة الالف ";
+                            break;
+                            case 6:
                             case 7://بالمليون
                             case 8:
                             case 9:
